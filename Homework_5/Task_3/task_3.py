9e777c03e261851a52d0515daade3cd814c832c1
Vitalii K_M <cj_vizer@ukr.net>
2021-02-11 23:32:27 +0200

    Completed task 3

--- Homework_5/Task_3/task_3.py
+++ Homework_5/Task_3/task_3.py
@@ -1,3 +1,15 @@
 """
 Получить файл, в котором текст выровнен по правому краю путем равномерного добавления пробелов.
 """
+
+with open('poem.txt', 'r', encoding='utf-8') as poem:
+    with open('poem_rjust.txt', 'w', encoding='utf-8') as poem_rjust:
+        max_length = 0
+        for line in poem:
+            line_length = len(line)
+            if line_length > max_length:
+                max_length = line_length
+
+        poem.seek(0)
+        for line in poem:
+            poem_rjust.write(line.rjust(max_length))
