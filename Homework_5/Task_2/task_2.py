da9be0e7de1a3fe1d65ef2b87e9ce8cf17b672f1
Vitalii K_M <cj_vizer@ukr.net>
2021-02-11 23:13:02 +0200

    Completed task 2

--- Homework_5/Task_2/task_2.py
+++ Homework_5/Task_2/task_2.py
@@ -2,3 +2,9 @@
 Текстовый файл содержит записи о телефонах и их владельцах.
 Переписать в другой файл телефоны тех владельцев, фамилии которых начинаются с букв К и С.
 """
+
+with open('db.txt', 'r', encoding='utf-8') as db_read:
+    with open('task_db.txt', 'w', encoding='utf-8') as db_write:
+        for item in db_read:
+            if item.startswith(('К', 'С')):
+                db_write.write(item.split(',')[-1])
