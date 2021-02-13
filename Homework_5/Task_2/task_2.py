7c0730ebb5dce5ca88b08b1a09f8627185e9ab49
Vitalii K_M <cj_vizer@ukr.net>
2021-02-13 09:18:33 +0200

    Task_2: A new function get_phone_numbers_last_name_startswith has been created. Added check for the presence of an input file.

--- Homework_5/Task_2/task_2.py
+++ Homework_5/Task_2/task_2.py
@@ -3,8 +3,24 @@
 Переписать в другой файл телефоны тех владельцев, фамилии которых начинаются с букв К и С.
 """
 
-with open('db.txt', 'r', encoding='utf-8') as db_read:
-    with open('task_db.txt', 'w', encoding='utf-8') as db_write:
-        for item in db_read:
-            if item.startswith(('К', 'С')):
-                db_write.write(item.split(',')[-1])
+
+def get_phone_numbers_last_name_startswith(in_file, out_file, *args):
+    """
+    This function creates a new file with phone numbers of owners
+    whose last names begin with the letters specified in args.
+    :param in_file: Input file name (input type: str)
+    :param out_file: Output file name (input type: str)
+    :param args: First letters of surnames (input type: str)
+    :return: None
+    """
+    try:
+        with open(in_file, 'r', encoding='utf-8') as db_read:
+            with open(out_file, 'w', encoding='utf-8') as db_write:
+                for item in db_read:
+                    if item.startswith(args):
+                        db_write.write(item.split(',')[-1])
+    except FileNotFoundError:
+        print(f'File with the name "{in_file}" does not exist')
+
+
+get_phone_numbers_last_name_startswith('db.txt', 'task_db.txt', 'К', 'С')
