2a5b3bfca790bbb8abdfb8f75eca19d5112d7372
Vitalii K_M <cj_vizer@ukr.net>
2021-02-13 09:55:18 +0200

    Task_3: A new function align_right has been created. Added check for the presence of an input file.

--- Homework_5/Task_3/task_3.py
+++ Homework_5/Task_3/task_3.py
@@ -2,14 +2,28 @@
 Получить файл, в котором текст выровнен по правому краю путем равномерного добавления пробелов.
 """
 
-with open('poem.txt', 'r', encoding='utf-8') as poem:
-    with open('poem_rjust.txt', 'w', encoding='utf-8') as poem_rjust:
-        max_length = 0
-        for line in poem:
-            line_length = len(line)
-            if line_length > max_length:
-                max_length = line_length
 
-        poem.seek(0)
-        for line in poem:
-            poem_rjust.write(line.rjust(max_length))
+def align_right(in_file, out_file):
+    """
+    This function right-aligns text in a file
+    :param in_file: Input file name (input type: str)
+    :param out_file: Output file name (input type: str)
+    :return: None
+    """
+    try:
+        with open(in_file, 'r', encoding='utf-8') as input_file:
+            with open(out_file, 'w', encoding='utf-8') as output_file:
+                max_length = 0
+                for line in input_file:
+                    line_length = len(line)
+                    if line_length > max_length:
+                        max_length = line_length
+
+                input_file.seek(0)
+                for line in input_file:
+                    output_file.write(line.rjust(max_length))
+    except FileNotFoundError:
+        print(f'File with the name "{in_file}" does not exist')
+
+
+align_right('poem.txt', 'poem_rjust.txt')
