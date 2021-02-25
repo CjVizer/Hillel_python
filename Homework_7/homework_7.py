584c9014c9f0d3cd3789e09d1c0e30e623046889
Vitalii K_M <cj_vizer@ukr.net>
2021-02-25 19:08:18 +0200

    Bugs fixed

--- Homework_7/homework_7.py
+++ Homework_7/homework_7.py
@@ -12,18 +12,22 @@ from contextlib import contextmanager
 
 class ToDirectory:
     def __init__(self, directory_path, suppress_exc_flag=None):
-        self.directory_path = directory_path
-        self.suppress_exc_flag = suppress_exc_flag
+        self._directory_path = directory_path
+        self._suppress_exc_flag = suppress_exc_flag
 
     def __enter__(self):
-        self.parent_dir = os.getcwd()
-        os.chdir(self.directory_path)
+        self._parent_dir = os.getcwd()
+        try:
+            os.chdir(self._directory_path)
+        except self._suppress_exc_flag or Exception:
+            exc_type, exc_val, exc_tb = sys.exc_info()
+            if self._suppress_exc_flag == exc_type:
+                print(f'Suppressed exception: {exc_type}, {exc_val}')
+            else:
+                raise
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        os.chdir(self.parent_dir)
-        if self.suppress_exc_flag and exc_type == self.suppress_exc_flag:
-            print(f'Suppressed exception: {exc_type}, {exc_val}')
-            return True
+        os.chdir(self._parent_dir)
 
 
 """
@@ -35,17 +39,17 @@ class ToDirectory:
 @contextmanager
 def to_directory(directory_path, suppress_exc_flag=None):
     parent_dir = os.getcwd()
-    os.chdir(directory_path)
     try:
+        os.chdir(directory_path)
         yield
-    except Exception as err:
+    except suppress_exc_flag or Exception:
         exc_type, exc_val, exc_tb = sys.exc_info()
-        if suppress_exc_flag and exc_type == suppress_exc_flag:
+        if suppress_exc_flag == exc_type:
             print(f'Suppressed exception: {exc_type}, {exc_val}')
+            yield
         else:
             raise
-    finally:
-        os.chdir(parent_dir)
+    os.chdir(parent_dir)
 
 
 """
