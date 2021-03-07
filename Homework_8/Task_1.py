0fa2b80c2740f72e2a9fbecc58121cf536b956f0
Vitalii K_M <cj_vizer@ukr.net>
2021-03-07 12:50:23 +0200

    Changed regular expression for email validation

--- Homework_8/Task_1.py
+++ Homework_8/Task_1.py
@@ -3,7 +3,7 @@
 # Ваш дескриптор должен проверять формат email который вы пытаетесь назначить
 import re
 
-pattern = re.compile("[A-Za-z0-9]+@[a-z.]+")
+pattern = re.compile('^[_A-Za-z0-9-+]+[\._A-Za-z0-9-]*@[A-Za-z0-9-]+[\.[A-Za-z0-9]+]*$')
 
 
 class EmailDescriptor:
